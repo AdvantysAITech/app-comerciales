a6aadbe92b48a8f80288663745bd47c43e2fa490
AdvantysAITech <soporte@jacobruiz.es>
2026-09-15 10:49:23 +0200

    chore: dejar de versionar salida/ y vars.ps1

--- vars.ps1
+++
@@ -1,4 +0,0 @@
-$Api = "https://prlia.solucionait.es"
-$Dir = "C:\Users\Alex Ruiz\OneDrive\Escritorio\app-comerciales-banff"
-$Ghl = "https://services.leadconnectorhq.com"
-$OpId = "FmMI2lwn7JJOpWIJ9NeF"
